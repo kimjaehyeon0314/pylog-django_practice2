e136552ccfc0d102fc153bc2644e6a03bcaf16b9
Jaehyeon Kim <syukjh0314@naver.com>
2024-01-06 23:02:19 +0900

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from config.views import index
+from blog.views import post_list, post_detail, post_add
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", index),
+    path("posts/", post_list),
+    path("posts/<int:post_id>/", post_detail),
+    path("posts/add/", post_add),
+]
+
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
