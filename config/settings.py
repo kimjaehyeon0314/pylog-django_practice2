5c5060dd7be52bffd69913418dc42e8ec7a3e5fc
Jaehyeon <114378755+kimjaehyeon0314@users.noreply.github.com>
2024-03-08 21:12:33 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-yakvf%(r507dzgb_ypg)8((x_wd2@r(024hlb)%p5f06d*o(f*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['61.109.239.46', 'localhost', '127.0.0.1']
 
 
 # Application definition
