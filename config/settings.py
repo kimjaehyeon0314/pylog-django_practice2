fc949732edf4f4570ee3cfd7bae2e9bcf2353535
Jaehyeon <114378755+kimjaehyeon0314@users.noreply.github.com>
2024-03-09 18:11:21 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / "app" / "staticfiles"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "app" / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
